788c0865df22c8c94d883181b4c261a2880495a6
Johan Rydell <johannilsrydell89@gmail.com>
2024-02-24 17:10:41 +0100

    bug fix

--- src/Components/MonsterBox/MonsterBox.jsx
+++ src/Components/MonsterBox/MonsterBox.jsx
@@ -11,27 +11,26 @@ const MonsterBox = () => {
 
   const [monsters, setMonsters] = useState(initialMonsters);
 
-  useEffect(() => {
-    // Sort monsters whenever the state is updated
-    const sortedMonsters = [...monsters].sort((a, b) => b.kills - a.kills);
-    setMonsters(sortedMonsters);
-  }, [monsters]);
-
   return (
     <>
       <div className="MonsterBox">
         <h1>Leaderboard</h1>
         {monsters.map((monster, index) => (
-        <Monster
-         key={index}
-         name={monster.name}
-         image={monster.image}
-         kills={monster.kills}
-        
-         setKills={(newKills) => {
+          <Monster
+            key={index}
+            name={monster.name}
+            image={monster.image}
+            kills={monster.kills}
+            setKills={(newKills) => {
               const updatedMonsters = [...monsters];
-              updatedMonsters[index].kills = newKills;
-              setMonsters(updatedMonsters);
+              if (newKills != updatedMonsters[index].kills) {
+                updatedMonsters[index].kills = newKills;
+
+                const sortedMonsters = [...monsters].sort(
+                  (a, b) => b.kills - a.kills
+                );
+                setMonsters(sortedMonsters);
+              }
             }}
           />
         ))}
